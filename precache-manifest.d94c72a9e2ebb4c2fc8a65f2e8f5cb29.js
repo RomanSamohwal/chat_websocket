6266dee0676888042e8f36fcda7100892ed16240
RomanSamohwal <rsamohwal@gmail.com>
2020-10-18 21:42:51 +0300

    Updates

---
+++ precache-manifest.d94c72a9e2ebb4c2fc8a65f2e8f5cb29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc80b40612c0df3804cb13489a35139",
+    "url": "/chat_websocket/index.html"
+  },
+  {
+    "revision": "45ebe1e632c715e60224",
+    "url": "/chat_websocket/static/css/main.d26b98d1.chunk.css"
+  },
+  {
+    "revision": "9cfee233b1d08ff639e1",
+    "url": "/chat_websocket/static/js/2.c29dbd39.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/chat_websocket/static/js/2.c29dbd39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45ebe1e632c715e60224",
+    "url": "/chat_websocket/static/js/main.ddcd2d11.chunk.js"
+  },
+  {
+    "revision": "ca47e30fe3885c42a762",
+    "url": "/chat_websocket/static/js/runtime-main.25a12a3d.js"
+  }
+]);
